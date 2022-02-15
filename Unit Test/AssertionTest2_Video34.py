61e8be50161e6e0381d1a390a80bdb5422ac6344
mddmustainbillah <19103007@iubat.edu>
2022-02-15 16:01:10 +0600

    assertTrue/assertFalse

--- Unit Test/AssertionTest2_Video34.py
+++ Unit Test/AssertionTest2_Video34.py
@@ -1,3 +1,21 @@
+############ assertTrue & assertFalse #############
+
+##### assertTrue
+# When we have only two parameter we can us assertEqual method to check whether both are
+# same or not, but if we have more than two parameters, comparing values with assertEqual
+# method become more dificult.
+#
+# assertTrue method checks whether given parameter is true or not, if value is true then
+# test is passed otherwise test is failed.
+
+##### assertFalse
+# assertFalse method compares whether given value or expression results in false or not
+#
+# If the result or value is False then unittest passes the testcase but if the result
+# or value is True then unittest fails the test case.
+
+
+
 import unittest
 from selenium import webdriver
 
