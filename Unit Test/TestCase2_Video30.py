f1fc7dcaff1ecc69c23d2748907a36859cd109d0
mddmustainbillah <19103007@iubat.edu>
2022-02-15 12:01:47 +0600

    UnitTest2

---
+++ Unit Test/TestCase2_Video30.py
@@ -0,0 +1,20 @@
+import unittest
+from selenium import webdriver
+
+
+class SearchEnginesTest(unittest.TestCase):
+    def test_Google(self):
+        self.driver = webdriver.Chrome(executable_path="C:\Drivers\chromedriver_win32\chromedriver.exe")
+        self.driver.get("https://www.google.com/")
+        print("Title of the page is: " + self.driver.title)
+        self.driver.close()  # have to close the browser
+
+    def test_Bing(self):
+        self.driver = webdriver.Chrome(executable_path="C:\Drivers\chromedriver_win32\chromedriver.exe")
+        self.driver.get("https://bing.com")
+        print("Title of the page is :" + self.driver.title)
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
