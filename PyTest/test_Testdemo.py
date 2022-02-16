bdfb3f1f11ce5113e3f9553e19c5992a273ecdc3
mddmustainbillah <19103007@iubat.edu>
2022-02-16 15:28:57 +0600

    pytest

--- PyTest/test_Testdemo.py
+++ PyTest/test_Testdemo.py
@@ -2,6 +2,10 @@
 # pytest -v -s
 # pytest -v -s test_Testdemo.py
 
+# When using pytest, it is very important PyTest Naming Conventions.
+# If we don't follow naming conventions then it will not pick up tests from the file.
+# File names should start as test_example.py or example_test.py
+
 
 import pytest
 
